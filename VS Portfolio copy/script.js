293ac87f88663f5233656fb20b6aa4a1458bb26c
Afrah <122495808+Aas3@users.noreply.github.com>
2024-05-07 18:19:45 -0700

    test1

---
+++ VS Portfolio copy/script.js
@@ -0,0 +1,37 @@
+(function () {
+    'use strict';
+
+///////////////////////////////////////////////////////  PASSWORD ///
+
+    // var greeting = function(){
+    //     var userAnswer = prompt('enter password', "");
+
+
+    //     userAnswer=userAnswer.toLowerCase();
+
+
+    //     if(userAnswer === "hi"){
+    //         console.log("HI! its nice to meet you!  °˖✧◝(⁰▿⁰)◜✧˖° ");
+    //         for(var i=0; i=== userAnswer.length ; i++){
+    //             console.log(`hi ${i}`);
+    //         }
+    //     }
+    //     else if(userAnswer === "" ){
+    //         console.log("Youre leaving already ... bye ( ˃̣̣̥⌓˂̣̣̥) (ुŏ̥̥̥̥ ‸ ŏ̥̥̥̥) ु");
+    //     }
+    //     else{
+    //         console.log("try one of the options instead ¯\_(ツ)_/¯ ");
+    //         console.log(`lengthOfString: ${userAnswer.length}`)
+    //     }
+ 
+    //     }
+    //     greeting();
+
+    
+
+///////////////////////////////////////////////////////  ON CLICK NAV ///
+
+
+
+
+})();
